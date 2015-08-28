31329f7203e360a9a2925408e8b099d611dc1b8c
William Baldwin <williambaldwin@Jesses-iMac.local>
2015-08-28 10:19:06 -0600

    Implemented zoom function for Sunburst graph.

--- pub/viewer.js
+++ pub/viewer.js
@@ -432,32 +432,37 @@ window.exports.viewer = (function () {
           .attr("d", arc)
           .attr("stroke", '#fff')
           .style("fill", function(d) { return color((d.children ? d : d.parent).key); });
-        /*if(graphs[counter].zoom){
+        if(graphs[counter].zoom){
           path.on("click", click);
           function click(d) {
-            text.transition().attr("opacity", 0);
+            if(text){
+              text.transition().attr("opacity", 0);
+            }
+            function tw(d){
+              var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+                  yd = d3.interpolate(y.domain(), [d.y, 1]),
+                  yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+              return function(d, i){
+                return i
+                    ? function (t) { return arc(d); }
+                    : function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+              };
+            }
 
             path.transition()
               .duration(750)
-              .attrTween("d", function(d){
-                var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
-                    yd = d3.interpolate(y.domain(), [d.y, 1]),
-                    yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
-                return function(d, i){
-                  return i ? function(t) { return arc(d); } : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
-                };
-              })
+              .attrTween("d", tw(d))
               .each("end", function(e, i){//when the transition ends
                 if(e.x >= d.x && e.x < (d.x + d.dx)) {//grab all the text
                   var arcText = d3.select(this.parentNode).select("text");
                   arcText.transition().duration(750)
                     .attr("opacity", 1)
-                    .attr("transform", function() { return "rotate(" + ((x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180) + ")";})
+                    .attr("transform", function() { return "rotate(" + ((x(e.x + e.dx / 2) - Math.PI / 2) / Math.PI * 180) + ")"})
                     .attr("x", function(d) { return y(d.y); });
                 }
               });
           }
-        }*/
+        }
         if(graphs[counter].labelling){
           var text = svg.append("text")
             .attr("transform", function(d) { return "rotate(" + ((x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180) + ")";})
